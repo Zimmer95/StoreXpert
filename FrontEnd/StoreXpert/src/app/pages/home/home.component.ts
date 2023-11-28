07a49b90b32379a63d7534626e3289db48d354f2
zimmer95 <jorgezimmer95@gmail.com>
2023-11-27 23:07:26 -0300

    Cambiando colores, agregando modelos de tiendas, modificando visual

--- FrontEnd/StoreXpert/src/app/pages/home/home.component.ts
+++ FrontEnd/StoreXpert/src/app/pages/home/home.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { GaleriaImgComponent } from "../../components/galeria-img/galeria-img.component";
 
 @Component({
-  selector: 'app-home',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+    selector: 'app-home',
+    standalone: true,
+    templateUrl: './home.component.html',
+    styleUrl: './home.component.css',
+    imports: [CommonModule, GaleriaImgComponent]
 })
 export class HomeComponent {
 
